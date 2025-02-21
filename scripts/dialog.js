1fde69d93adb02bca1410124ca47ec237057f232
Brooke-8 <bmacq014@uottawa.ca>
2025-02-20 22:25:32 -0500

    lol so many images

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -14,17 +14,32 @@ for (let i = 0; i < projects.length; i++){
 /* Image Dialog */
 const photoDialog = document.getElementById("photodialog");
 const imageForDialog = document.getElementById("photodialogimage");
-const images = document.getElementsByClassName("openableimage");
-
-for (let i = 0; i < images.length; i++){
-    images.item(i).onclick = function(){
-        imageForDialog.src = images.item(i).src;
-        photoDialog.showModal();
+async function loadImages(){
+    const list = await fetch('images/pixelart/pixelartimages.json');
+    const images = await list.json();
+    const imagecontainer = document.getElementById("pixelimagecontainer");
+    let imgHTML = '';
+    images.forEach(imagePath =>{
+        imgHTML += `<img class="pixelimage openableimage" src="${imagePath}">`;
+    })
+    imagecontainer.innerHTML = `${imgHTML}`;
+    imageListeners();
+}   
+function imageListeners(){
+    const images = document.getElementsByClassName("openableimage");
+    for (let i = 0; i < images.length; i++){
+        images.item(i).onclick = function(){
+            imageForDialog.src = images.item(i).src;
+            photoDialog.showModal();
+        }
     }
 }
+loadImages();
+imageListeners();
 
+
+/* Closing Dialogs */
 window.addEventListener("click",closeDialog);
-
 function closeDialog(event){
     if (event.target == dialog){
         dialog.close();
@@ -32,5 +47,8 @@ function closeDialog(event){
     }   
     if (event.target == photoDialog){
         photoDialog.close();
+        imageForDialog.src="";
     }
 }
+
+
